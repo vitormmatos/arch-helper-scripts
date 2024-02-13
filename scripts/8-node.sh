58af380db3c7486107af6a3dc35afbbfac08d479
Vítor Matos <vitormmatos@proton.me>
2024-02-13 20:17:48 -0300

    path set for node 18 installed with mise

--- scripts/8-node.sh
+++ scripts/8-node.sh
@@ -3,6 +3,9 @@ paru -S mise-bin --skipreview
 mise install node@18 node@20
 mise use --global node@18
 
+echo 'NODE_INSTALL="/home/vitor/.local/share/mise/installs/node/18"' >> ~/.zshrc
+echo 'export PATH="$NODE_INSTALL/bin:$PATH" >> ~/.zshrc'
+
 # Bun
 curl -fsSL https://bun.sh/install | bash
 exec /usr/bin/zsh
