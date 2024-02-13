f062e6325fc37432afc6a6237c1e381ae4969075
Vítor Matos <vitormmatos@proton.me>
2024-02-13 20:18:57 -0300

    breaking typo fixed
    again

--- scripts/8-node.sh
+++ scripts/8-node.sh
@@ -4,7 +4,7 @@ mise install node@18 node@20
 mise use --global node@18
 
 echo 'NODE_INSTALL="/home/vitor/.local/share/mise/installs/node/18"' >> ~/.zshrc
-echo 'export PATH="$NODE_INSTALL/bin:$PATH"' >> ~/.zshrc'
+echo 'export PATH="$NODE_INSTALL/bin:$PATH"' >> ~/.zshrc
 
 # Bun
 curl -fsSL https://bun.sh/install | bash
