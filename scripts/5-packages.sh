c78567fcedaf30c2e856304b37e4b713eb09001d
Vítor Matos <vitormmatos@proton.me>
2024-03-29 19:16:07 -0300

    spotify package added
    documentation added as well

--- scripts/5-packages.sh
+++ scripts/5-packages.sh
@@ -1,5 +1,8 @@
+# Installs a few packages for day=to-day usage
+
 paru -S --skipreview micro \
     brave-bin \
+    spotify-launcher \
     transmission-gtk3 \
     simplenote-electron-bin \
     gnome-shell-extension-dash-to-dock \
