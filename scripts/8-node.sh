6dc6df28dae0646c7b965d25f89aa36f803f6f21
Vítor Matos <vitormmatos@proton.me>
2024-03-29 19:14:12 -0300

    node env path updated
    documentation added

--- scripts/8-node.sh
+++ scripts/8-node.sh
@@ -1,11 +1,17 @@
-# Mise
+# Installs Mise, a version manager for NodeJS
+# For more info visit:
+# https://mise.jdx.dev/
+
 paru -S mise-bin --skipreview
 mise install node@18 node@20
 mise use --global node@18
 
-echo 'NODE_INSTALL="/home/vitor/.local/share/mise/installs/node/18"' >> ~/.zshrc
+echo 'NODE_INSTALL="~/.local/share/mise/installs/node/18"' >> ~/.zshrc
 echo 'export PATH="$NODE_INSTALL/bin:$PATH"' >> ~/.zshrc
 
-# Bun
+# Installs Bun, an all-in-one JS toolkit
+# For more info visit:
+# https://bun.sh/ 
+
 curl -fsSL https://bun.sh/install | bash
 exec /usr/bin/zsh
