ccba61866c8337ef7780ec0c00275871cb27002a
Vítor Matos <vitormmatos@proton.me>
2024-03-29 19:15:30 -0300

    p10k install updated to manual
    documentation added

--- scripts/4-zsh.sh
+++ scripts/4-zsh.sh
@@ -1,17 +1,22 @@
+# Install zsh for shell
 paru -S zsh zsh-completions zsh-syntax-highlighting --skipreview
+
+# Config Oh My Zsh 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Icon font for pk10 theme
-paru -S ttf-meslo-nerd-font-powerlevel10k zsh-theme-powerlevel10k-git --skipreview
+paru -S ttf-meslo-nerd-font-powerlevel10k --skipreview
 
-echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
-p10k configure
+# Config pk10
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.local/powerlevel10k
+echo 'source ~/.local/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
 # Shortcuts
 echo '#Shortcuts' >> ~/.zshrc
 echo 'alias sync="sudo pacman -Syyy"' >> ~/.zshrc
 echo 'alias install="paru -S --skipreview"' >> ~/.zshrc
 echo 'alias update="paru -Su --skipreview"' >> ~/.zshrc
+echo 'alias upgrade="paru -Su --skipreview"' >> ~/.zshrc
 echo 'alias search="sudo pacman -Ss"' >> ~/.zshrc
 echo 'alias search-local="sudo pacman -Qs"' >> ~/.zshrc
 echo 'alias pkg-info="sudo pacman -Qi"' >> ~/.zshrc
