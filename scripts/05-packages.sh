2d710b3ac368a332b6fb739497a245662e2b3392
Vítor Matos <vitormmatos@proton.me>
2024-05-19 17:38:25 -0300

    blackbox terminal added

--- scripts/05-packages.sh
+++ scripts/05-packages.sh
@@ -1,9 +1,10 @@
-# Installs a few packages for day=to-day usage
+# Installs a few packages for day-to-day usage
 
 paru -S --skipreview micro \
     brave-bin \
     spotify-launcher \
     transmission-gtk3 \
+    blackbox-terminal \
     simplenote-electron-bin \
     gnome-shell-extension-dash-to-dock \
     
